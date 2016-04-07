e01811ddf193ddb737883fdc8bd94acc55580c2f
Cristian Cortez <cortez.cristian@gmail.com>
2016-04-07 20:21:25 -0300

    Sitio real

---
+++ advanced-demos/sitio-real/app/scripts/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sitioRealApp
+ * @description
+ * # sitioRealApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('sitioRealApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ advanced-demos/sitio-real/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name sitioRealApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the sitioRealApp
+ */
+angular.module('sitioRealApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
